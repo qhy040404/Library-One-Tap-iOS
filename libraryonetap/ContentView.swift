23b89f299ab8102f1ac0de3a6fa19f1a980696e6
qhy040404 <qhy040404@163.com>
2022-04-24 18:14:53 +0800

    Initial Commit

---
+++ libraryonetap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  libraryonetap
+//
+//  Created by qhy on 2022/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ libraryonetap/libraryonetapApp.swift
@@ -0,0 +1,17 @@
+//
+//  libraryonetapApp.swift
+//  libraryonetap
+//
+//  Created by qhy on 2022/4/24.
+//
+
+import SwiftUI
+
+@main
+struct libraryonetapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
